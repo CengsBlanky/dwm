d370134b135cd6377ff143a1fb72a9fe356756f1
zengshuai <zengs1994@gmail.com>
2025-07-05 22:27:32 +0800

    update config file

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosevkaZeng:size=18", "monospace:size=12" };
-static const char dmenufont[]       = "IosevkaZeng:size=20";
+static const char *fonts[]          = { "IosevkaZeng:size=16", "monospace:size=12" };
+static const char dmenufont[]       = "IosevkaZeng:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,12 +57,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "/usr/local/bin/tmux", NULL };
 static const char *browser[]  = { "firefox-esr", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {.v = browser } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
