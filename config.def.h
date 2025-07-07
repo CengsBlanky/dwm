1dcd7b850882bf58b125b1c336e0f6d577f3fbd9
zengshuai <zengs1994@gmail.com>
2025-07-08 00:20:17 +0800

    update dwm colors

--- config.def.h
+++ config.def.h
@@ -2,15 +2,15 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 2;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosevkaZeng:size=14", "monospace:size=12" };
+static const char *fonts[]          = { "OpenSans:size=14", "monospace:size=12" };
 static const char dmenufont[]       = "CourierPrime:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#cbc093";
+static const char col_gray4[]       = "#dcd7ba";
 static const char col_cyan[]        = "#2a303d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "6", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[]  = { "firefox-esr", NULL };
 
