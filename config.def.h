469733f89c7a51594085141e81deda64948001cf
zengshuai <zengs1994@gmail.com>
2025-10-21 13:37:27 +0800

    update font config

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // use font original name instead of fontconfig alias
 /* emoji font must be at the second place to take effect!!! */
-static const char *fonts[]          = { "Noto Sans:size=11", "Noto Color Emoji:size=11", "Noto Sans SC:size=11", "Arimo Nerd Font:size=11", };
+static const char *fonts[]          = { "Noto Sans:size=11", "Noto Color Emoji:size=11", "Arimo Nerd Font:size=11", "Noto Sans SC:size=11", };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#868e96";
